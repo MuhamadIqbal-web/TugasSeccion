ffe2c8f36f5d6684b1330d378434ae84720a8172
MuhamadIqbal-web <markbytrade@gmail.com>
2025-10-15 23:19:34 +0700

    Create user.php

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(!isset($_SESSION['unique_id']) || $_SESSION['role'] != 'user'){
+    header("location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Anda user</h1>
+     <form action="logouttugas.php" method="post">
+        <button type="submit" name="logout">Logout</button>
+</body>
+</html>
