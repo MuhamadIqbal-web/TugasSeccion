dd7a2b7b3ed3582775eb2d15d58e0397e909a022
MuhamadIqbal-web <markbytrade@gmail.com>
2025-10-15 23:16:50 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(!isset($_SESSION['unique_id']) || $_SESSION['role'] != 'admin'){
+    header("location: login.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Anda admin</h1>
+
+       <form action="logouttugas.php" method="post">
+        <button type="submit" name="logout">Logout</button>
+    </form>
+</body>
+</html>
